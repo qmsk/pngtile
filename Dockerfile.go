f4254b5e315976de46e3d4aa8204daa966a08fad
Tero Marttila <terom@fixme.fi>
2017-07-16 16:04:00 +0300

    Dockerfile.go

---
+++ Dockerfile.go
@@ -0,0 +1,16 @@
+FROM debian:stretch
+
+RUN apt-get update && apt-get install -y \
+    build-essential libc6-dev \
+    libpng16-16 libpng-dev \
+    golang-go git
+
+RUN mkdir -p /go
+ENV GOPATH=/go
+RUN go get -v -d github.com/urfave/cli
+
+ADD . /go/src/github.com/qmsk/pngtile
+WORKDIR /go/src/github.com/qmsk/pngtile
+
+RUN make
+RUN go install -v ./go/...
