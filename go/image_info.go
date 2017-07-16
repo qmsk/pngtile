c2df25def457b7c1523d5fd9319152c94c682453
Tero Marttila <terom@fixme.fi>
2017-07-16 22:27:33 +0300

    go pngtile: fixup ImageInfo: ImageFormat, CacheFormat

--- go/image_info.go
+++ go/image_info.go
@@ -14,12 +14,14 @@ func makeTime(t C.time_t) time.Time {
 
 func makeImageInfo(ci *C.struct_pt_image_info) ImageInfo {
 	return ImageInfo{
+		ImageFormat:       ImageFormat(ci.image_format),
 		ImageWidth:        uint(ci.image_width),
 		ImageHeight:       uint(ci.image_height),
 		ImageBPP:          uint(ci.image_bpp),
 		ImageModifiedTime: makeTime(ci.image_mtime),
 		ImageBytes:        uint(ci.image_bytes),
 		CacheVersion:      int(ci.cache_version),
+		CacheFormat:       ImageFormat(ci.cache_format),
 		CacheModifiedTime: makeTime(ci.cache_mtime),
 		CacheBytes:        uint(ci.cache_bytes),
 		CacheBlocks:       uint(ci.cache_blocks),
@@ -33,6 +35,7 @@ type ImageInfo struct {
 	ImageBPP          uint        `json:"image_bpp"`
 	ImageModifiedTime time.Time   `json:"image_mtime"`
 	ImageBytes        uint        `json:"image_bytes"`
+	CacheFormat       ImageFormat `json:"cache_format"`
 	CacheVersion      int         `json:"cache_version"`
 	CacheModifiedTime time.Time   `json:"cache_mtime"`
 	CacheBytes        uint        `json:"cache_bytes"`
